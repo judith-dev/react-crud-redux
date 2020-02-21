80f91db8dc7389b8e76e5894bb34f9bc7d00c1c3
@judith-dev <judith.develop@gmail.com>
2020-02-21 06:46:13 -0600

    implementa redux

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
+import {transactionReducer} from "./reducers/transactionReducer";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (localStorage.getItem('transactions') == null)
+    localStorage.setItem('transactions', JSON.stringify([]))
+let initialState = {
+    currentIndex: -1,
+    list: JSON.parse(localStorage.getItem('transactions'))
+}
+const store = createStore(transactionReducer, initialState)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
